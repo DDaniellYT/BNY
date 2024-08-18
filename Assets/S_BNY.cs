38338ad2d0c5bf7d966a9fe480e89279c3c644ce
DDaniellYT <ocrisan23@gmail.com>
2024-08-18 18:46:28 +0300

    path creator working ; path following rough implementation

--- Assets/S_BNY.cs
+++ Assets/S_BNY.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class S_BNY : MonoBehaviour{
-    [SerializeField] public Material pointMaterial;
-    static public List<List<GameObject>> pathQueue = new List<List<GameObject>>();
+    static public List<List<Vector3>> pathQueue = new List<List<Vector3>>();
     static public float health = 100f;
     static public float hunger = 100f;
     static public float speed = 10f;
+    static public float maxDistPerc = 0.8f;
 }
